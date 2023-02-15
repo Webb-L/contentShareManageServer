35b1bcf70b82d2d64316c7265eadb1681bed855c
webb <822028533@qq.com>
2023-02-15 23:41:26 +0800

    feat(数据): 新增设备相关功能。

--- controllers/device_controller.go
+++ controllers/device_controller.go
@@ -2,28 +2,117 @@ package controllers
 
 import (
 	model "contentShareManage/models"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
-func Index(context *gin.Context) {
+// DeviceIndex @title 查询设备
+// @description 查询当前有多少设备
+func DeviceIndex(context *gin.Context) {
 	devices := model.GetDevices()
-	context.JSON(http.StatusOK, devices)
+	startString, exists := context.GetQuery("start")
+	if !exists {
+		startString = "0"
+	}
+	endString, exists := context.GetQuery("end")
+	if !exists {
+		endString = "10"
+	}
+
+	start, err := strconv.Atoi(startString)
+	if err != nil {
+		start = 0
+	}
+
+	end, err := strconv.Atoi(endString)
+	if err != nil {
+		end = 10
+	}
+
+	if start > len(devices) {
+		start = 0
+	}
+
+	if end > len(devices) {
+		end = len(devices)
+	}
+
+	resultDevice := make([]model.Device, 0)
+	for _, device := range devices[start:end] {
+		device.Token = ""
+		device.Password = ""
+		resultDevice = append(resultDevice, device)
+	}
+
+	context.JSON(http.StatusOK, resultDevice)
 }
 
-func Store(context *gin.Context) {
+// DeviceStore @title 新增设备
+// @description 保存新的设备。如果是第一台设备连接设置为管理员。
+// @param context *gin.Context
+func DeviceStore(context *gin.Context) {
 	device := model.Device{}
 	context.BindJSON(&device)
+	firstDevice := model.Size() == 0
+	device.IsAdmin = firstDevice
+	device.IsRead = firstDevice
+	device.IsSend = true
+	device.Token = model.BuildToken()
+	if model.ExistDevice(device.Name) {
+		context.String(http.StatusAccepted, "设备已存在！")
+		return
+	}
+	model.AddDevice(device)
 	context.JSON(http.StatusOK, device)
 }
 
-func Show(context *gin.Context) {
-	fmt.Println(context.Param("name"))
+// DeviceShow @title 查询设备信息
+// @description 显示设备名称。
+func DeviceShow(context *gin.Context) {
+	tempDevice := model.QueryDevice(context.Param("name"))
+	if tempDevice == nil {
+		context.String(http.StatusNotFound, "找不到设备！")
+		return
+	}
+	device := tempDevice.(model.Device)
+	device.Token = ""
+	device.Password = ""
+	context.JSON(http.StatusOK, device)
 }
 
-// Destroy @title 删除设备
-// @description 删除设备（管理员权限）。
-func Destroy(context *gin.Context) {
+// DeviceUpdate @title 更新设备
+// @description 更新设备信息。
+func DeviceUpdate(context *gin.Context) {
+	tempDevice, exists := context.Get("device")
+	if !exists {
+		context.String(http.StatusNotFound, "找不到设备！")
+		return
+	}
 
+	// 找到设备
+	device := tempDevice.(model.Device)
+	newDevice := model.Device{}
+	context.BindJSON(&newDevice)
+	newDevice.Name = device.Name
+	newDevice.Token = device.Token
+	model.UpdateDevice(newDevice)
+	context.String(http.StatusOK, "更新成功！")
+}
+
+// DeviceDestroy @title 删除设备
+// @description 删除设备。
+func DeviceDestroy(context *gin.Context) {
+	tempDevice, exists := context.Get("device")
+	if !exists {
+		context.String(http.StatusNotFound, "找不到设备！")
+		return
+	}
+
+	device := tempDevice.(model.Device)
+	if model.DeleteDevice(device.Name, device.Password) {
+		context.String(http.StatusOK, "删除成功！")
+	} else {
+		context.String(http.StatusNotFound, "找不到设备！")
+	}
 }
--- router/device_router.go
+++ router/device_router.go
@@ -2,18 +2,72 @@ package router
 
 import (
 	controller "contentShareManage/controllers"
-	"fmt"
+	model "contentShareManage/models"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func InitDeviceRouter(r *gin.Engine) {
 	deviceGroup := r.Group("/device", func(context *gin.Context) {
-		fmt.Println("/device 中间件")
+		queryPassword, exists := context.GetQuery("password")
+		if !exists || queryPassword != password {
+			context.String(http.StatusUnauthorized, "密码错误！")
+			context.Abort()
+			context.Next()
+			return
+		}
+
+		if context.Request.Method != "GET" && context.Request.Method != "POST" {
+			// 检测Token 是否有效。
+			token := context.GetHeader("Token")
+			if token == "" {
+				context.String(http.StatusUnauthorized, "您没有权限操作！")
+				context.Abort()
+				context.Next()
+				return
+			}
+			temp := model.QueryDeviceByToken(token)
+			if temp == nil {
+				context.String(http.StatusUnauthorized, "您没有权限操作！")
+				context.Abort()
+				context.Next()
+				return
+			}
+
+			// 判断Token的用户是否有权限操作。
+			device := temp.(model.Device)
+			if !device.IsAdmin {
+				context.String(http.StatusForbidden, "您没有权限操作！")
+				context.Abort()
+				context.Next()
+				return
+			}
+
+			// 查询被操作的设备
+			name := context.Param("name")
+			if name == "" {
+				context.String(http.StatusNotFound, "找不到设备！")
+				context.Abort()
+				context.Next()
+				return
+			}
+
+			queryDevice := model.QueryDevice(name)
+			if queryDevice == nil {
+				context.String(http.StatusNotFound, "找不到设备！")
+				context.Abort()
+				context.Next()
+				return
+			}
+
+			context.Set("device", queryDevice.(model.Device))
+		}
 	})
 	{
-		deviceGroup.GET("/", controller.Index)
-		deviceGroup.POST("/", controller.Store)
-		deviceGroup.GET("/:name", controller.Show)
-		deviceGroup.DELETE("/:name", controller.Destroy)
+		deviceGroup.GET("/", controller.DeviceIndex)
+		deviceGroup.POST("/", controller.DeviceStore)
+		deviceGroup.GET("/:name", controller.DeviceShow)
+		deviceGroup.PUT("/:name", controller.DeviceUpdate)
+		deviceGroup.DELETE("/:name", controller.DeviceDestroy)
 	}
 }
